da74519f9e0625a6ce386081488447c637c1555b
st <rougsig@gmail.com>
2018-09-17 20:28:54 +0200

    Add transition fix for HideButton

--- app/src/main/java/ru/rougsig/funwithanimations/shopcard/ShopCardChangeHandler.kt
+++ app/src/main/java/ru/rougsig/funwithanimations/shopcard/ShopCardChangeHandler.kt
@@ -4,6 +4,7 @@ import android.transition.*
 import android.view.View
 import android.view.ViewGroup
 import com.bluelinelabs.conductor.changehandler.SharedElementTransitionChangeHandler
+import kotlinx.android.synthetic.main.shop_card_controller.view.*
 import ru.rougsig.funwithanimations.R
 
 class ShopCardChangeHandler : SharedElementTransitionChangeHandler() {
@@ -15,10 +16,28 @@ class ShopCardChangeHandler : SharedElementTransitionChangeHandler() {
   }
 
   override fun getSharedElementTransition(container: ViewGroup, from: View?, to: View?, isPush: Boolean): Transition? {
+
+    val hideButton = if (isPush) {
+      from!!.shop_card_hide_button
+    } else {
+      to!!.shop_card_hide_button
+    }
+    hideButton.visibility = View.INVISIBLE
+
     return TransitionSet()
       .addTransition(ChangeBounds())
       .addTransition(ChangeClipBounds())
       .addTransition(ChangeTransform())
+      .addListener(object : Transition.TransitionListener {
+        override fun onTransitionEnd(transition: Transition?) {
+          hideButton.visibility = View.VISIBLE
+        }
+
+        override fun onTransitionResume(transition: Transition?) = Unit
+        override fun onTransitionPause(transition: Transition?) = Unit
+        override fun onTransitionCancel(transition: Transition?) = Unit
+        override fun onTransitionStart(transition: Transition?) = Unit
+      })
   }
 
   override fun getEnterTransition(container: ViewGroup, from: View?, to: View?, isPush: Boolean): Transition? {
